b3ee266420033783e7e900a2b1385d600310a867
engrjohnai <143824735+engrjohnai@users.noreply.github.com>
2024-07-08 17:04:22 +0100

    Create Class.py

---
+++ Class.py
@@ -0,0 +1,14 @@
+class Dog:
+    species = "Canis Familiaris"
+    def __init__(self, name, age, size):
+        self.name = name
+        self.age = age
+        self.size = size
+
+    def description(self):
+        return f"{self.name} is {self.age} years old that is {self.size} lbs in weight"
+
+    def speak(self, sound):
+        return f"{self.name} says {self.sound}"
+
+
